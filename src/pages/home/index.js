26717047cade27af228fed14e048a1c9b5c0f3b9
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2024-03-21 23:21:44 +0530

    Implemented add functionality host

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const AddForm = React.lazy(() => import("addForm/addForm"));
 const FilterForm = React.lazy(() => import("filterSection/filterSection"));
 const List = React.lazy(() => import("list/list"));
 import { ActionBar, AddFormSection, CardsListSection, ListTitle, MainContainer, SearchControllSection } from "./style";
 
 const Home = () => {
+    const [todoData, setTodoData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => { 
+        fetch(`https://jsonplaceholder.typicode.com/todos`)
+            .then((res) => res.json())
+            .then((result) => {
+                setTodoData(result);
+                setLoading(false)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+  
+    const handleAdd = (e, todoTitle) => {
+        setTodoData((prev) => [
+            {
+                userId: new Date().getTime(),
+                id: new Date().getTime(),
+                title: todoTitle,
+                completed: false,
+                isDummy: true
+            },
+            ...prev
+        ])
+        e.preventDefault()
+    }
+
+    if(loading) return <span style={{color:"red"}}>Loading...</span>
 
     return (
         <MainContainer>
             <ActionBar>
                 <AddFormSection>
-                    <AddForm/>
+                    <AddForm handleAdd={handleAdd}/>
                 </AddFormSection>
                 <SearchControllSection>
                     <FilterForm/>
@@ -19,7 +49,7 @@ const Home = () => {
             <>
                 <ListTitle>Todo List :</ListTitle>
                 <CardsListSection>
-                    <List/>
+                    <List todoData={todoData}/>
                 </CardsListSection>
             </>
         </MainContainer>
